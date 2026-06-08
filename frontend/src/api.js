01506507d01a335034ac87912b8eeae1b835f35e
unknown <Faiz Al Islam@RFSD372888.btn.co.id>
2026-06-09 01:22:45 +0700

    Connect frontend to Railway API

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://medis-website2-production.up.railway.app/api"
 });
 
 api.interceptors.request.use((config) => {
